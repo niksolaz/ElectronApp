4a817a0f89f112c93aa4d1a0eda54bc4ee3b3644
niksolaz <solazzo.nicola@gmail.com>
2016-03-06 18:44:31 +0100

    added new event from google developers

--- quickstart.js
+++ quickstart.js
@@ -1,12 +1,17 @@
 'use strict';
 var fs = require('fs');
+var smalltalk = require('smalltalk');
 var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/calendar-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+/*
+changed SCOPE 'https://www.googleapis.com/auth/calendar.readonly' with
+new path to add events 'https://www.googleapis.com/auth/calendar';
+*/
+var SCOPES = ['https://www.googleapis.com/auth/calendar'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
@@ -22,6 +27,44 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
   authorize(JSON.parse(content), listEvents);
 });
 
+// example Json file in https://developers.google.com/google-apps/calendar  event --> insert
+var event = {
+  'summary': 'Google I/O 2015',
+  'location': '800 Howard St., San Francisco, CA 94103',
+  'description': 'A chance to hear more about Google\'s developer products.',
+  'start': {
+    'dateTime': '2015-05-28T09:00:00-07:00',
+    'timeZone': 'America/Los_Angeles'
+  },
+  'end': {
+    'dateTime': '2015-05-28T17:00:00-07:00',
+    'timeZone': 'America/Los_Angeles'
+  },
+  'recurrence': [
+    'RRULE:FREQ=DAILY;COUNT=2'
+  ],
+  'attendees': [
+    {'email': 'lpage@example.com'},
+    {'email': 'sbrin@example.com'}
+  ],
+  'reminders': {
+    'useDefault': false,
+    'overrides': [
+      {'method': 'email', 'minutes': 24 * 60},
+      {'method': 'popup', 'minutes': 10}
+    ]
+  }
+};
+console.log('event',event);
+var request = gapi.client.calendar.events.insert({
+  'calendarId': 'primary',
+  'resource': event
+});
+console.log('request', request)
+request.execute(function(event) {
+  appendPre('Event created: ' + event.htmlLink);
+});
+
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
  * given callback function.
@@ -30,14 +73,18 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
+  console.log("CREDENTIALS", credentials);
   var clientSecret = credentials.installed.client_secret;
   var clientId = credentials.installed.client_id;
   var redirectUrl = credentials.installed.redirect_uris[0];
   var auth = new googleAuth();
   var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+  console.log("OAUTH2", oauth2Client);
 
   // Check if we have previously stored a token.
+  console.log("TOKEN PATH", TOKEN_PATH);
   fs.readFile(TOKEN_PATH, function(err, token) {
+    console.log("TOKEN PATH RESULT", err, token);
     if (err) {
       getNewToken(oauth2Client, callback);
     } else {
@@ -56,17 +103,38 @@ function authorize(credentials, callback) {
  *     client.
  */
 function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
+  try{
+    var authUrl = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES
+    });
+  }catch(err){
+    console.log("Errr", err);
+  }
   console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+
+  document.write(`Go to this <a href='${authUrl}'>url</a> and copy the code in the text field below`);
+  smalltalk.prompt('Question', 'Enter the code from that page here: ', '').then(function(code) {
+    console.log("code", code);
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  }, function() {
+      console.log('cancel');
   });
-  rl.question('Enter the code from that page here: ', function(code) {
+  /*
+  console.log("rl.question", rl.question);
+  rl.prompt("test", function(abc){console.log(abc)});
+  rl.question('Enter the code from that page here: ', (code) => {
+    console.log("Answer", code);
     rl.close();
+    console.log("CODE", code);
     oauth2Client.getToken(code, function(err, token) {
       if (err) {
         console.log('Error while trying to retrieve access token', err);
@@ -77,6 +145,7 @@ function getNewToken(oauth2Client, callback) {
       callback(oauth2Client);
     });
   });
+  */
 }
 
 /**
@@ -117,14 +186,18 @@ function listEvents(auth) {
     }
     var events = response.items;
     if (events.length == 0) {
+      document.write('No upcoming events');
       console.log('No upcoming events found.');
     } else {
-      console.log('Upcoming 10 events:');
+      document.write(`Upcoming ${events.length} events`);
+      document.write("<ul>");
       for (var i = 0; i < events.length; i++) {
         var event = events[i];
         var start = event.start.dateTime || event.start.date;
         console.log('%s - %s', start, event.summary);
+        document.write(`<li><a href='${event.htmlLink}'>Event: ${start} - ${event.summary}</a></li>`);
       }
+      document.write('</ul>')
     }
   });
 }
